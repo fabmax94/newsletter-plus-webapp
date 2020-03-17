fa4a15098b1a74c82e1799bd459a48c81a9b9ce0
fabio.silva <fabio.silva@samsung.com>
2020-03-17 15:29:24 -0400

    update gitignore

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -7,7 +7,6 @@ import Icon from "@material-ui/core/Icon";
 import People from "@material-ui/icons/People";
 // core components
 import Header from "components/Header/Header.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
